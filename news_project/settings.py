add313d652c911a2fd0753740193a7ecf8e9631f
shohqosim <100335712+shohqosimqahhorov@users.noreply.github.com>
2024-01-18 17:40:40 +0300

    51 darsgacha qilindi

--- news_project/settings.py
+++ news_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'newsapp',
+    'hitcount',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home_page'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_URL = 'login'
--- newsapp/urls.py
+++ newsapp/urls.py
@@ -9,16 +9,26 @@ from .views import (
     PoliticNewsView,
     TechNewsView,
     SportNewsView,
-    SocietyNewsView
+    SocietyNewsView,
+    NewsUpdateViews,
+    NewsDeleteView,
+    NewsCreateView,
+    admin_page_view,
+    SearchResultsList
 )
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
     path('news/', news_list, name='all_news_list'),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
     path('news/<slug:news>/', news_detail, name='news_detail'),
+    path('news/<slug>/edit/', NewsUpdateViews.as_view(), name='news_update'),
+    path('news/<slug>/delete/', NewsDeleteView.as_view(), name='news_delete'),
     path('contact-us/', ContactPageView.as_view(), name='contact_page'),
     path('local/', LocalNewsView.as_view(), name='local_news'),
     path('politic/', PoliticNewsView.as_view(), name='politic_news'),
     path('tech/', TechNewsView.as_view(), name='tech_news'),
     path('sport/', SportNewsView.as_view(), name='sport_news'),
-    path('society/', SocietyNewsView.as_view(), name='society_news')
+    path('society/', SocietyNewsView.as_view(), name='society_news'),
+    path('adminpage/', admin_page_view, name='admin_page'),
+    path('searchresult/', SearchResultsList.as_view(), name='search_results'),
 ]
